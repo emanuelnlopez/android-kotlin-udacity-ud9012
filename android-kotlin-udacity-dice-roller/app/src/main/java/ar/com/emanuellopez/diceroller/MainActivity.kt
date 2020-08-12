628b61d08cec51d018ad44b34bc8e98af0262d7a
Emanuel López <emanuel.lopez@globant.com>
2020-08-12 18:35:09 -0300

    + Added logic to show a random number when button clicked

--- android-kotlin-udacity-dice-roller/app/src/main/java/ar/com/emanuellopez/diceroller/MainActivity.kt
+++ android-kotlin-udacity-dice-roller/app/src/main/java/ar/com/emanuellopez/diceroller/MainActivity.kt
@@ -2,10 +2,24 @@ package ar.com.emanuellopez.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
