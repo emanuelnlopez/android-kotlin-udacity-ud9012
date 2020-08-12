442307af109c11a8a9c51acbc664357e3b1c8337
Emanuel López <emanuel.lopez@globant.com>
2020-08-12 18:41:28 -0300

    + Replaced TextView with an ImageView showing the corresponding dice face

--- android-kotlin-udacity-dice-roller/app/src/main/java/ar/com/emanuellopez/diceroller/MainActivity.kt
+++ android-kotlin-udacity-dice-roller/app/src/main/java/ar/com/emanuellopez/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package ar.com.emanuellopez.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -18,8 +19,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
