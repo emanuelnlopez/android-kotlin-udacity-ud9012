a0ed90cc826589a45b576e8f7d3987f63bba864a
Emanuel López <emanuel.lopez@globant.com>
2020-08-14 19:06:04 -0300

    + Replaced onStart and onStop for starting and stopping the timer for Lifecycle library

--- android-kotlin-udacity-dessert-pusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ android-kotlin-udacity-dessert-pusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
-        dessertTimer = DessertTimer()
+        dessertTimer = DessertTimer(this.lifecycle)
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -155,7 +155,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         super.onStart()
-        dessertTimer.startTimer()
         Timber.i("onStart called")
     }
 
@@ -176,7 +175,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         super.onStop()
-        dessertTimer.stopTimer()
         Timber.i("onStop called")
     }
 
