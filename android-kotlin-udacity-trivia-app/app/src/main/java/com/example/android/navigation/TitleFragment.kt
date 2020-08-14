14ce8c41803a1bce9b7882f358cea322886179cd
Emanuel López <emanuel.lopez@globant.com>
2020-08-14 09:47:57 -0300

    + Added TitleFragment

---
+++ android-kotlin-udacity-trivia-app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,18 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_title, container, false)
+        return binding.root
+    }
+}
