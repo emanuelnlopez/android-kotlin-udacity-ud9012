6a61af92e5a59108328094aaf4b4d298c48d1033
Emanuel López <emanuel.lopez@globant.com>
2020-08-14 18:19:55 -0300

    + Replaced Android Log with Timber

--- android-kotlin-udacity-dessert-pusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ android-kotlin-udacity-dessert-pusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleObserver
 import com.example.android.dessertpusher.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity(), LifecycleObserver {
 
@@ -65,7 +66,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("MainActivity", "onCreate called")
+        Timber.i("onCreate called")
 
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -151,6 +152,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         super.onStart()
-        Log.i("MainActivity", "onStart called")
+        Timber.i("onStart called")
     }
 }
