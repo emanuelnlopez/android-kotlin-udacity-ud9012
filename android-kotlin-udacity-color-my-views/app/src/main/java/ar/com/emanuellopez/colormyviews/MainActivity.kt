dc6745a2b2cdd42429703ed4234a4675385be279
Emanuel López <emanuel.lopez@globant.com>
2020-08-14 08:56:53 -0300

    + Added OnClickListeners to change the boxes colors

--- android-kotlin-udacity-color-my-views/app/src/main/java/ar/com/emanuellopez/colormyviews/MainActivity.kt
+++ android-kotlin-udacity-color-my-views/app/src/main/java/ar/com/emanuellopez/colormyviews/MainActivity.kt
@@ -1,11 +1,43 @@
 package ar.com.emanuellopez.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout)
+
+        for(item in clickableViews) {
+            item.setOnClickListener{
+                makeColored(it)
+            }
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
