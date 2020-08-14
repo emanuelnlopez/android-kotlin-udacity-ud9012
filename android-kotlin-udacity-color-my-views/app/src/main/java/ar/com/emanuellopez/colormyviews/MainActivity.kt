77d67a4351d103ae46b61147ae778c737a5afd9a
Emanuel López <emanuel.lopez@globant.com>
2020-08-14 09:13:33 -0300

    + Added 3 buttons with constraints and updated OnClickListener

--- android-kotlin-udacity-color-my-views/app/src/main/java/ar/com/emanuellopez/colormyviews/MainActivity.kt
+++ android-kotlin-udacity-color-my-views/app/src/main/java/ar/com/emanuellopez/colormyviews/MainActivity.kt
@@ -15,11 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, constraint_layout)
+            listOf(
+                box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout,
+                red_button, yellow_button, green_button
+            )
 
-        for(item in clickableViews) {
-            item.setOnClickListener{
+        for (item in clickableViews) {
+            item.setOnClickListener {
                 makeColored(it)
             }
         }
@@ -36,6 +39,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
