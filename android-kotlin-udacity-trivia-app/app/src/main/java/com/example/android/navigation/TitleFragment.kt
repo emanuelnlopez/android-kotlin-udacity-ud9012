e63858e92d7f7103960cee361222f654cda58237
Emanuel López <emanuel.lopez@globant.com>
2020-08-14 10:10:40 -0300

    + Added navigation component

--- android-kotlin-udacity-trivia-app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ android-kotlin-udacity-trivia-app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,13 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-class TitleFragment: Fragment() {
+class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
                 R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 }
