4b0ef327ba6328e18408b98a07ab903678e472ae
Emanuel López <emanuel.lopez@globant.com>
2020-08-18 16:20:38 -0300

    + Update layout view using data binding

--- andfun-kotlin-udacity-guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ andfun-kotlin-udacity-guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -54,14 +54,7 @@ class GameFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
         binding.gameViewModel = viewModel
-
-        viewModel.score.observe(this, Observer{ newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
-
-        viewModel.word.observe(this, Observer{ newWord ->
-            binding.wordText.text = newWord
-        })
+        binding.setLifecycleOwner(this)
 
         viewModel.currentTime.observe(this, Observer{ newTime ->
             binding.timerText.text = DateUtils.formatElapsedTime(newTime)
