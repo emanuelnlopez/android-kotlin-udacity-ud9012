eca556c9d41bcf74c3bcf5ec1684759b93558453
Emanuel López <emanuel.n.lopez@gmail.com>
2020-08-20 12:16:24 -0300

    + Show formatted sleep data

--- android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -1,23 +1,12 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.trackmysleepquality
 
 import android.annotation.SuppressLint
 import android.content.res.Resources
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import androidx.core.text.HtmlCompat
+import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
 
 /**
@@ -69,32 +58,32 @@ fun convertLongToDateString(systemTime: Long): String {
  * @return  Spanned - An interface for text that has formatting attached to it.
  *           See: https://developer.android.com/reference/android/text/Spanned
  */
-//fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
-//    val sb = StringBuilder()
-//    sb.apply {
-//        append(resources.getString(R.string.title))
-//        nights.forEach {
-//            append("<br>")
-//            append(resources.getString(R.string.start_time))
-//            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
-//            if (it.endTimeMilli != it.startTimeMilli) {
-//                append(resources.getString(R.string.end_time))
-//                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
-//                append(resources.getString(R.string.quality))
-//                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
-//                append(resources.getString(R.string.hours_slept))
-//                // Hours
-//                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
-//                // Minutes
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
-//                // Seconds
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
-//            }
-//        }
-//    }
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
-//    } else {
-//        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
-//    }
-//}
+fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
+    val sb = StringBuilder()
+    sb.apply {
+        append(resources.getString(R.string.title))
+        nights.forEach {
+            append("<br>")
+            append(resources.getString(R.string.start_time))
+            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
+            if (it.endTimeMilli != it.startTimeMilli) {
+                append(resources.getString(R.string.end_time))
+                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
+                append(resources.getString(R.string.quality))
+                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
+                append(resources.getString(R.string.hours_slept))
+                // Hours
+                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
+                // Minutes
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
+                // Seconds
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
+            }
+        }
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+}
--- android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -3,8 +3,10 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
 import kotlinx.coroutines.*
 
 /**
@@ -26,6 +28,10 @@ class SleepTrackerViewModel(
     private var tonight = MutableLiveData<SleepNight?>()
 
     private val nights = database.getAllNights()
+
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
 
     init {
         initializeTonight()
