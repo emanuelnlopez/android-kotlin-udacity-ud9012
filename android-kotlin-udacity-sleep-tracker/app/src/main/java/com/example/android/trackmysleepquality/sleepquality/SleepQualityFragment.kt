71f1807ff3403358142770d4be1bd69d5cf16e77
Emanuel López <emanuel.n.lopez@gmail.com>
2020-08-20 16:18:13 -0300

    + Update SleepNight with Sleep Quality

--- android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepQualityBinding
 
 /**
@@ -30,6 +34,26 @@ class SleepQualityFragment : Fragment() {
                 inflater, R.layout.fragment_sleep_quality, container, false)
 
         val application = requireNotNull(this.activity).application
+
+        val arguments = SleepQualityFragmentArgs.fromBundle(arguments!!)
+
+        val datasource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey, datasource)
+
+        val sleepQualityViewModel = ViewModelProvider(this,  viewModelFactory)
+                .get(SleepQualityViewModel::class.java)
+
+        binding.sleepQualityViewModel = sleepQualityViewModel
+
+        sleepQualityViewModel.navigateToSleepTracker.observe(this, Observer {
+            if (it == true) {
+                this.findNavController().navigate(
+                        SleepQualityFragmentDirections
+                                .actionSleepQualityFragmentToSleepTrackerFragment())
+                sleepQualityViewModel.doneNavigating()
+            }
+        })
 
         return binding.root
     }
