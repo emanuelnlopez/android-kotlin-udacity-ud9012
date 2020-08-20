ccf3514f99245a709842f0209cd33c8134e83722
Emanuel López <emanuel.n.lopez@gmail.com>
2020-08-20 09:14:56 -0300

    - Removed unneeded parameter un function signature

--- android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ android-kotlin-udacity-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -38,7 +38,7 @@ interface SleepDatabaseDao {
     fun clear()
 
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
-    fun getAllNights(key: Long): LiveData<List<SleepNight>>
+    fun getAllNights(): LiveData<List<SleepNight>>
 
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
     fun getTonight(): SleepNight?
