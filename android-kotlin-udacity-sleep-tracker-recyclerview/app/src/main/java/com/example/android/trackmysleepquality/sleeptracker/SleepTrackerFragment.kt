3c4a33f15b7151235eb9211c83226d3b72b5b716
Emanuel López <emanuel.n.lopez@gmail.com>
2020-10-06 18:51:51 -0300

    + Added a header to the gridlist

--- android-kotlin-udacity-sleep-tracker-recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ android-kotlin-udacity-sleep-tracker-recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -81,7 +81,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.submitList(it)
+                adapter.addHeaderAndSubmitList(it)
             }
 
         })
