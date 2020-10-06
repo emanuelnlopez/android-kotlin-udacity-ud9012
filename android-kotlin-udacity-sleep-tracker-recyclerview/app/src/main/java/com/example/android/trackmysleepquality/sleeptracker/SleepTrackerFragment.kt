0ca78d44dab80076664f53a57ca721d3cc12a84d
Emanuel López <emanuel.n.lopez@gmail.com>
2020-10-06 18:54:35 -0300

    + Display header in the complete first row of the gridlist

--- android-kotlin-udacity-sleep-tracker-recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ android-kotlin-udacity-sleep-tracker-recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -64,6 +64,12 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object: GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int)= when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
         binding.sleepList.layoutManager = manager
 
         val adapter = SleepNightAdapter(SleepNightListener { nightId ->
